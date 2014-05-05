8985305faa4639df97aac0a3c5bfb4e1ce585a33
richardabc <richardabc@gmail.com>
2014-05-05 02:03:36 -0700

    setting the config for heroku and extract the data out

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 var express = require('express'),
     earthquake = require('./routes/earthquakes');
- 
+
 var app = express();
- 
+var port = process.env.PORT || 3000;
+
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
+    app.use(function(req, res, next) {
+        earthquake.initDatabase(function() {
+            next();
+        });
+    });
 });
- 
+
 app.get('/earthquakes.json', earthquake.findAll);
 app.get('/earthquakes.json/:id', earthquake.findById);
 app.post('/earthquakes.json', earthquake.addEarthquake);
 app.put('/earthquakes.json/:id', earthquake.updateEarthquake);
 app.delete('/earthquakes.json/:id', earthquake.deleteEarthquake);
- 
-app.listen(3000);
-console.log('Listening on port 3000...');
+
+app.listen(port);
+console.log('Server started, listening on port ' + port + '....');
